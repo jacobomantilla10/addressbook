70ffa4df9a216da07be55c76d21c26c43efdb561
jacobo mantilla <jacobomantilla10@gmail.com>
2025-10-14 13:47:44 -0400

    Serve static files and add scripts for dev and start.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '../public')))
+
 app.get("/", (req, res) => {
     console.log("get request");
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
 app.listen(port, () => {
